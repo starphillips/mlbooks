6a305b018b38969ef381c8ded083ecd31fa5c0cf
Starkie Phillips <star.phillips03@gmail.com>
2024-11-11 17:48:59 +0000

    Inital Commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template("index.html")
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
