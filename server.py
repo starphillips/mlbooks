baefd420afbb0fa13e4605fc0c532ece984b250b
Starkie Phillips <star.phillips03@gmail.com>
2024-12-01 00:01:24 +0000

    website info and form submission

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -11,3 +12,27 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        csv_writer = csv.writer(database, delimiter=',',
+                                quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            print(data)
+            return redirect('index.html')
+        except:
+            return 'Unable to save to database'
+    else:
+        return 'Error in Sending. Please try again.'
